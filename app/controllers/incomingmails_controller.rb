afb4be5fa366264cc50f642c03b52e6f470b634e
Sean <sean.t.irby@gmail.com>
2013-06-06 01:15:07 -0700

    Add debug output to incomingmails controller

--- app/controllers/incomingmails_controller.rb
+++ app/controllers/incomingmails_controller.rb
@@ -5,12 +5,18 @@ class IncomingmailsController < ApplicationController
     #yank out body from message
     body = params[:plain]
 
+    Rails.logger.log Logger::INFO, params
+    Rails.logger.log Logger::INFO, body.class
+    Rails.logger.log Logger::INFO, body.methods
+    Rails.logger.log Logger::INFO, body
+
+
     #yank out ticket id from message_
     ticket_id = body.lines[0].chomp
     ticket = Ticket.find_by_id(ticket_id)
 
     if ticket
-      ticket.update_attribute(response, message.body)
+      ticket.update_attribute(response, body)
       render :text => 'success', :status => 200
     end
   end
