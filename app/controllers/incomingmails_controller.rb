8dc7378d6c2583f10429b248e8fa73fd6e1fb930
Sean <sean.t.irby@gmail.com>
2013-06-06 00:57:23 -0700

    Fix mails controller again

--- app/controllers/incomingmails_controller.rb
+++ app/controllers/incomingmails_controller.rb
@@ -2,15 +2,13 @@ class IncomingmailsController < ApplicationController
   require 'mail'
 
   def update
-    #Search
-    message = Mail.new(params[:message])
-
     #yank out body from message
-    ticket_id = message.body.lines[0].chomp
+    body = params[:plain]
+
     #yank out ticket id from message_
-
-
+    ticket_id = body.lines[0].chomp
     ticket = Ticket.find_by_id(ticket_id)
+
     if ticket
       ticket.update_attribute(response, message.body)
       render :text => 'success', :status => 200
