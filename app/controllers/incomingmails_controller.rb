f0618ba127043f7e287559eed0176f7724c8fdb9
Aaron Trostle <aaron.trostle@gmail.com>
2013-06-06 19:31:29 -0700

    Match ticket id from email subject, temporarily use cloudmailin env variable for heroku test, update email message

--- app/controllers/incomingmails_controller.rb
+++ app/controllers/incomingmails_controller.rb
@@ -11,14 +11,19 @@ class IncomingmailsController < ApplicationController
     Rails.logger.log Logger::INFO, body
 
 
-    #yank out ticket id from message_
-    ticket_id = body.lines.first.chomp
+    #yank out ticket id from subject
+    ticket_id = get_ticket_id(params[:headers][:Subject])
     ticket = Ticket.find_by_id(ticket_id)
 
     if ticket
       ticket.update_attribute(:response, body)
       render :text => 'success', :status => 200
     end
+  end
+
+private
+  def get_ticket_id(param)
+    param.match(/<(\d*)>/)[1]
   end
 
 end
